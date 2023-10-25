ea4892378b19970438a07de1afffd09c4901f177
MAK <97058316+mak360@users.noreply.github.com>
2023-10-26 01:04:46 +0600

    init

---
+++ wp-test-plugin.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+ * Plugin Name:      WP Test Plugin
+ * Plugin URI:        https://github.com/mak360
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mak
+ * Author URI:        https://github.com/mak360
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       wp-testplugin
+ * Domain Path:       /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/** */
